0a3691db6242a73b1aefd2a21b8f3d1ae8327a4e
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-07-09 00:28:59 +0530

    Add: created seperate file to define a book names,price and created one entity class for defining book details

---
+++ src/test/java/com/sdb/tdd/SoftwareDevlopmentBookList.java
@@ -0,0 +1,16 @@
+package com.sdb.tdd;
+
+import com.sdb.entity.SoftwareDevelopmentBook;
+
+public class SoftwareDevlopmentBookList {
+
+	public static SoftwareDevelopmentBook GivenASoftwareDevelopmentIBook() {
+		SoftwareDevelopmentBook book = new SoftwareDevelopmentBook("Clean Code (Robert Martin, 2008)");
+		return book;
+	}
+
+	public static SoftwareDevelopmentBook GivenASoftwareDevelopmentIIBook() {
+		SoftwareDevelopmentBook book = new SoftwareDevelopmentBook("The Clean Coder (Robert Martin, 2011)");
+		return book;
+	}	
+}
